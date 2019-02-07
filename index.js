ca8a52ad8b2c8102d85a241a6129abeaf197e0e8
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-02-07 19:34:44 +0200

    package update

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 console.log("Hello");
 
+var rp = require('request-promise');
+var sanitize = require("sanitize-filename");
+
 let urls = require("./urls");
 
-for (let i=0; i < urls.length; i++){
+const fs = require('fs');
 
-    let url = urls[i];
+async function process(urls){
 
-    console.log(url[0], url[1], url[2]);
+    for (let i=0; i < urls.length; i++){
+
+        let url = urls[i];
+
+        console.log( url[1]);
+
+        let trials = 0;
+        while (trials < 10){
+
+            try{
+                let out = await rp( "https://web.archive.org/web/" + url[1] );
+                //console.log(out);
+                if (!out) throw "invalid out";
+
+                let filename = url[0]+"_"+url[1];
+                filename = sanitize(filename);
+
+                filename = filename.replace("_httpluhan.ro80", "")
+                filename = filename.replace("_httpluhan.ro", "")
+
+                await fs.writeFileSync("./pages/"+filename+".html", out );
+
+                break;
+
+            } catch (exception){
+            }
+
+            trials ++;
+        }
+
+    }
 
 }
+
+
+process(urls);
